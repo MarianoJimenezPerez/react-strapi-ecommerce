a3b8bd92830dab9550673fbaece5dc0e01cc4506
marianojimenezperez <marianojimenezperez@gmail.com>
2023-05-08 19:16:10 -0300

    Primer commit. Se empuja estructura del cliente

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/products/:category",
+        element: <Products />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
